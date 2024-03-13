4f3df313304fb317987ef524e5f17023a3d3852e
PowerfulBacon <26465327+PowerfulBacon@users.noreply.github.com>
2024-03-13 11:06:53 +0000

    Resizable radar component

--- tgui/packages/tgui/interfaces/NtosRadar.jsx
+++ tgui/packages/tgui/interfaces/NtosRadar.jsx
@@ -1,84 +1,31 @@
 import { classes } from 'common/react';
-
+import { Fragment, Component, createRef } from 'inferno';
 import { resolveAsset } from '../assets';
 import { useBackend } from '../backend';
-import {
-  Box,
-  Button,
-  Flex,
-  Icon,
-  NoticeBox,
-  Section,
-  Tooltip,
-} from '../components';
+import { Box, Button, Flex, Icon, NoticeBox, Section, Tooltip } from '../components';
 import { NtosWindow } from '../layouts';
 
-export const NtosRadar = (props) => {
-  const { act, data } = useBackend();
+export const NtosRadar = (props, context) => {
+  const { act, data } = useBackend(context);
   const { full_capability } = data;
   return (
-    <NtosWindow
-      width={full_capability ? 800 : 400}
-      height={full_capability ? 600 : 500}
-      theme="ntos"
-    >
-      {full_capability ? (
-        <NtosRadarContent sig_err={'Signal Lost'} />
-      ) : (
-        <NtosRadarContentSmall sig_err={'Signal Lost'} />
-      )}
+    <NtosWindow width={full_capability ? 800 : 400} height={full_capability ? 600 : 500} theme="ntos">
+      {full_capability ? <NtosRadarContent sig_err={'Signal Lost'} /> : <NtosRadarContentSmall sig_err={'Signal Lost'} />}
     </NtosWindow>
   );
 };
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
-export const NtosRadarContentSmall = (props) => {
-  const { act, data } = useBackend();
-  const {
-    selected,
-    object = [],
-    target = [],
-    scanning,
-    full_capability,
-  } = data;
+export const NtosRadarContentSmall = (props, context) => {
+  const { act, data } = useBackend(context);
+  const { selected, object = [], target = [], scanning, full_capability } = data;
   const { sig_err } = props;
   return (
     <NtosWindow.Content scrollable>
-      <Section>
-        {Object.keys(target).length === 0 ? (
-          selected ? (
-            <NoticeBox width={42} fontSize="30px" textAlign="center">
-              {sig_err}
-            </NoticeBox>
-          ) : (
-            <Box>No Target Selected.</Box>
-          )
-        ) : (
-          <Box>
-            Distance: {target.dist} {target.locz_string}{' '}
-            {target.use_rotate &&
-            target.pointer_z &&
-            target.pin_grand_z_result ? (
-              <Tooltip content={'WARNING: Target is too far away.'}>
-                <Icon name="exclamation-triangle" color="yellow" />
-              </Tooltip>
-            ) : null}
-            <br />
-            Location: ({target.gpsx}x, {target.gpsy}y, {target.gpsz}z){' '}
-            {target.use_rotate ? (
-              <Icon
-                name={target.dist > 0 ? 'arrow-up' : 'crosshairs'}
-                style={{
-                  transform: `rotate(${target.rotate_angle}deg)`,
-                }}
-              />
-            ) : null}{' '}
-            {target.use_rotate && target.pointer_z ? (
-              <Icon size={1.5} name={target.pointer_z} />
-            ) : null}
-          </Box>
-        )}
-      </Section>
+      <NtosRadarMap
+        selected={selected}
+        sig_err={sig_err}
+        target={target} />
 
       <Section>
         <Button
@@ -105,8 +52,7 @@ export const NtosRadarContentSmall = (props) => {
                 act('selecttarget', {
                   ref: object.ref,
                 });
-              }}
-            >
+              }}>
               {object.name}
             </div>
           ))}
@@ -115,15 +61,50 @@ export const NtosRadarContentSmall = (props) => {
   );
 };
 
-export const NtosRadarContent = (props) => {
-  const { act, data } = useBackend();
+export const NtosRadarMapSmall = (props, context) => {
   const {
-    selected,
-    object = [],
+    selected = false,
+    sig_err,
     target = [],
-    scanning,
-    full_capability,
-  } = data;
+  } = props;
+  return (
+    <Section>
+      {Object.keys(target).length === 0 ? (
+        selected ? (
+          <NoticeBox width={42} fontSize="30px" textAlign="center">
+            {sig_err}
+          </NoticeBox>
+        ) : (
+          <Box>No Target Selected.</Box>
+        )
+      ) : (
+        <Box>
+          Distance: {target.dist} {target.locz_string}{' '}
+          {target.use_rotate && target.pointer_z && target.pin_grand_z_result ? (
+            <Tooltip content={'WARNING: Target is too far away.'}>
+              <Icon name="exclamation-triangle" color="yellow" />
+            </Tooltip>
+          ) : null}
+          <br />
+          Location: ({target.gpsx}x, {target.gpsy}y, {target.gpsz}z){' '}
+          {target.use_rotate ? (
+            <Icon
+              name={target.dist > 0 ? 'arrow-up' : 'crosshairs'}
+              style={{
+                'transform': `rotate(${target.rotate_angle}deg)`,
+              }}
+            />
+          ) : null}{' '}
+          {target.use_rotate && target.pointer_z ? <Icon size={1.5} name={target.pointer_z} /> : null}
+        </Box>
+      )}
+    </Section>
+  );
+};
+
+export const NtosRadarContent = (props, context) => {
+  const { act, data } = useBackend(context);
+  const { selected, object = [], target = [], scanning, full_capability } = data;
   const { sig_err } = props;
   return (
     <Flex direction={'row'} hight="100%">
@@ -137,9 +118,7 @@ export const NtosRadarContent = (props) => {
               disabled={scanning}
               onClick={() => act('scan')}
             />
-            {!object.length && !scanning && (
-              <div>No trackable signals found</div>
-            )}
+            {!object.length && !scanning && <div>No trackable signals found</div>}
             {!scanning &&
               object.map((object) => (
                 <div
@@ -156,8 +135,7 @@ export const NtosRadarContent = (props) => {
                     act('selecttarget', {
                       ref: object.ref,
                     });
-                  }}
-                >
+                  }}>
                   {object.name}
                 </div>
               ))}
@@ -165,33 +143,84 @@ export const NtosRadarContent = (props) => {
         </NtosWindow.Content>
       </Flex.Item>
       <Flex.Item
-        style={{
-          backgroundImage:
-            'url("' + resolveAsset('ntosradarbackground.png') + '")',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          top: '20px',
-        }}
         position="relative"
         m={1.5}
         width={45}
         height={45}
-      >
-        {Object.keys(target).length === 0
-          ? !!selected && (
-              <NoticeBox
-                position="absolute"
-                top={20.6}
-                left={1.35}
-                width={42}
-                fontSize="30px"
-                textAlign="center"
-              >
+        style={{
+          'top': '20px',
+        }}>
+        <NtosRadarMap
+          selected={selected}
+          sig_err={sig_err}
+          target={target} />
+      </Flex.Item>
+    </Flex>
+  );
+};
+
+export class NtosRadarMap extends Component {
+  constructor(props) {
+    super(props);
+    this.containerRef = createRef();
+    this.state = {
+      width: 0,
+      height: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener('resize', this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  updateDimensions = () => {
+    const { width, height } = this.containerRef.current.getBoundingClientRect();
+    this.setState({
+      width: width,
+      height: height,
+    });
+  };
+
+  render()
+  {
+    const { sig_err, selected, target } = this.props;
+    const { width, height } = this.state;
+    const scalingFactor = (width < height ? width : height) / 540;
+    return (
+      <div
+        style={{
+          'position': 'absolute',
+          'top': 0,
+          'bottom': 0,
+          'left': 0,
+          'right': 0,
+          /* Important to make sure we don't get 540px of scrollbar */
+          'overflow': 'hidden',
+        }}
+        ref={this.containerRef}>
+        <div style={{
+          /* Render at a fixed width and height and then scale it */
+          'width': '540px',
+          'height': '540px',
+          'transform': 'scale(' + scalingFactor + ')',
+          'transform-origin': 'top left',
+          'background-image': 'url("' + resolveAsset('ntosradarbackground.png') + '")',
+          'background-position': 'center',
+          'background-repeat': 'no-repeat',
+        }}>
+          {Object.keys(target).length === 0
+            ? !!selected && (
+              <NoticeBox position="absolute" top={20.6} left={1.35} width={42} fontSize="30px" textAlign="center">
                 {sig_err}
               </NoticeBox>
             )
-          : (!!target.use_rotate && (
-              <>
+            : (!!target.use_rotate && (
+              <Fragment>
                 <Box
                   as="img"
                   src={resolveAsset(target.arrowstyle)}
@@ -199,7 +228,7 @@ export const NtosRadarContent = (props) => {
                   top="20px"
                   left="243px"
                   style={{
-                    transform: `rotate(${target.rotate_angle}deg)`,
+                    'transform': `rotate(${target.rotate_angle}deg)`,
                   }}
                 />
                 {target.pointer_z ? (
@@ -212,34 +241,29 @@ export const NtosRadarContent = (props) => {
                     left={224 + 'px'}
                   />
                 ) : null}
-              </>
+              </Fragment>
             )) || (
               <Icon
                 name={target.pointer_z ? target.pointer_z : 'crosshairs'}
                 position="absolute"
                 size={target.pointer_z ? 4 : 2}
-                color={
-                  target.pin_grand_z_result
-                    ? 'purple'
-                    : target.pointer_z
-                      ? 'orange'
-                      : target.color
-                }
+                color={target.pin_grand_z_result ? 'purple' : target.pointer_z ? 'orange' : target.color}
                 top={target.locy * 10 + 19 + 'px'}
                 left={target.locx * 10 + 16 + 'px'}
               />
             )}
-        <Box>
-          Distance: {target.dist} {target.locz_string}
-          {target.pointer_z && target.pin_grand_z_result ? (
-            <Tooltip content={'WARNING: Target is too far away.'}>
-              <Icon name="exclamation-triangle" color="yellow" />
-            </Tooltip>
-          ) : null}
-          <br />
-          Location: ({target.gpsx}x, {target.gpsy}y, {target.gpsz}z){' '}
-        </Box>
-      </Flex.Item>
-    </Flex>
-  );
-};
+          <Box>
+            Distance: {target.dist} {target.locz_string}
+            {target.pointer_z && target.pin_grand_z_result ? (
+              <Tooltip content={'WARNING: Target is too far away.'}>
+                <Icon name="exclamation-triangle" color="yellow" />
+              </Tooltip>
+            ) : null}
+            <br />
+            Location: ({target.gpsx}x, {target.gpsy}y, {target.gpsz}z){' '}
+          </Box>
+        </div>
+      </div>
+    );
+  }
+}
--- tgui/packages/tgui/interfaces/Uplink.jsx
+++ tgui/packages/tgui/interfaces/Uplink.jsx
@@ -1,24 +1,17 @@
 import { createSearch, decodeHtmlEntities } from 'common/string';
-
 import { useBackend, useLocalState } from '../backend';
-import {
-  Box,
-  Button,
-  Flex,
-  Input,
-  NoticeBox,
-  Section,
-  Table,
-  Tabs,
-  Tooltip,
-} from '../components';
+import { Stack, Box, Button, Flex, Input, Section, Table, Tabs, NoticeBox, Grid, Divider } from '../components';
 import { formatMoney } from '../format';
 import { Window } from '../layouts';
+import '../styles/components/Uplink.scss';
+import { NtosRadarSyndicate } from './NtosRadarSyndicate';
+import { NtosRadarContent, NtosRadarContentSmall, NtosRadarMap } from './NtosRadar';
+import { TurbineComputer } from './TurbineComputer';
 
 const MAX_SEARCH_RESULTS = 25;
 
-export const Uplink = (props) => {
-  const { data } = useBackend();
+export const Uplink = (props, context) => {
+  const { data } = useBackend(context);
   const { telecrystals } = data;
 
   const [tab, setTab] = useLocalState(context, 'tab_id', 0);
@@ -42,21 +35,102 @@ export const Uplink = (props) => {
             Priority Directives
           </Tabs.Tab>
         </Tabs>
-        {tab === 0 ? <GenericUplink currencyAmount={telecrystals} currencySymbol="TC" /> : 0}
+        {tab === 0 ? <GenericUplink currencyAmount={telecrystals} currencySymbol="TC" /> : (tab === 1 && <Directives />)}
       </Window.Content>
     </Window>
   );
 };
 
-export const GenericUplink = (props) => {
-  const { currencyAmount = 0, currencySymbol = 'cr' } = props;
-  const { act, data } = useBackend();
-  const { compactMode, lockable, categories = [] } = data;
-  const [searchText, setSearchText] = useLocalState('searchText', '');
-  const [selectedCategory, setSelectedCategory] = useLocalState(
-    'category',
-    categories[0]?.name,
+const Directives = (props, context) => {
+  const [selected, setSelected] = useLocalState(context, "sel_obj", 0);
+  return (
+    <Flex direction="column" className="directives">
+      <Flex.Item>
+        <Section title="Directives Database">
+          <Flex style={{
+            overflowY: "scroll",
+          }}>
+            <ObjectiveCard selected={selected === 0}
+              onClick={() => {
+                setSelected(0);
+              }} />
+            <ObjectiveCard selected={selected === 1}
+              onClick={() => {
+                setSelected(1);
+              }} />
+            <ObjectiveCard selected={selected === 2}
+              onClick={() => {
+                setSelected(2);
+              }} />
+            <ObjectiveCard selected={selected === 3} objective_info={{
+              name: "Secure Deposit",
+              reward: 7,
+              time_left: 349,
+            }}
+            onClick={() => {
+              setSelected(3);
+            }} />
+          </Flex>
+        </Section>
+      </Flex.Item>
+      <Flex.Item grow={1}>
+        <div className="directive_container">
+          <div className="directive_radar">
+            <NtosRadarMap sig_err="Error..." selected={1} target={{
+              dist: 53,
+              gpsx: 30,
+              gpsy: 60,
+              gpsz: 6,
+              use_rotate: true,
+              rotate_angle: 23,
+              arrowstyle: "ntosradarpointer.png",
+            }} />
+          </div>
+        </div>
+      </Flex.Item>
+    </Flex>
   );
+};
+
+const ObjectiveCard = (props, context) => {
+  const {
+    objective_info = {
+      name: "Assassination",
+      reward: 0,
+      time_left: null,
+    },
+    selected = 0,
+    onClick,
+  } = props;
+  const {
+    name,
+    reward,
+    time_left,
+  } = objective_info;
+  return (
+    <Flex.Item
+      className={"objective_card " + (selected && "selected")}
+      onClick={onClick}>
+      <Stack vertical>
+        <Stack.Item bold>{name}</Stack.Item>
+        <Stack.Divider />
+      </Stack>
+      <Box className="reward_overlay" align="flex-end" color={reward === 0 ? "orange" : "good"}>
+        {reward === 0 ? "Assignment" : (reward + " TC Reward")}
+      </Box>
+      <Box className="time_limit">
+        {time_left === null ? "--:--" : ("00:" + String(Math.floor(time_left / 60)).padStart(2, '0') + ":" + String(time_left % 60).padStart(2, '0'))}
+      </Box>
+    </Flex.Item>
+  );
+};
+
+export const GenericUplink = (props, context) => {
+  const { currencyAmount = 0, currencySymbol = 'cr' } = props;
+  const { act, data } = useBackend(context);
+  const { compactMode, lockable, categories = [] } = data;
+  const [searchText, setSearchText] = useLocalState(context, 'searchText', '');
+  const [selectedCategory, setSelectedCategory] = useLocalState(context, 'category', categories[0]?.name);
   const testSearch = createSearch(searchText, (item) => {
     return item.name + item.desc;
   });
@@ -81,23 +155,15 @@ export const GenericUplink = (props) => {
       buttons={
         <>
           Search
-          <Input
-            value={searchText}
-            autoFocus
-            onInput={(e, value) => setSearchText(value)}
-            mx={1}
-          />
+          <Input value={searchText} autoFocus onInput={(e, value) => setSearchText(value)} mx={1} />
           <Button
             icon={compactMode ? 'list' : 'info'}
             content={compactMode ? 'Compact' : 'Detailed'}
             onClick={() => act('compact_toggle')}
           />
-          {!!lockable && (
-            <Button icon="lock" content="Lock" onClick={() => act('lock')} />
-          )}
+          {!!lockable && <Button icon="lock" content="Lock" onClick={() => act('lock')} />}
         </>
-      }
-    >
+      }>
       <Flex>
         {searchText.length === 0 && (
           <Flex.Item>
@@ -106,8 +172,7 @@ export const GenericUplink = (props) => {
                 <Tabs.Tab
                   key={category.name}
                   selected={category.name === selectedCategory}
-                  onClick={() => setSelectedCategory(category.name)}
-                >
+                  onClick={() => setSelectedCategory(category.name)}>
                   {category.name} ({category.items?.length || 0})
                 </Tabs.Tab>
               ))}
@@ -116,11 +181,7 @@ export const GenericUplink = (props) => {
         )}
         <Flex.Item grow mx={2.5} basis={0}>
           {items.length === 0 && (
-            <NoticeBox>
-              {searchText.length === 0
-                ? 'No items in this category.'
-                : 'No results found.'}
-            </NoticeBox>
+            <NoticeBox>{searchText.length === 0 ? 'No items in this category.' : 'No results found.'}</NoticeBox>
           )}
           <ItemList
             compactMode={searchText.length > 0 || compactMode}
@@ -134,10 +195,10 @@ export const GenericUplink = (props) => {
   );
 };
 
-const ItemList = (props) => {
+const ItemList = (props, context) => {
   const { compactMode, currencyAmount, currencySymbol } = props;
-  const { act } = useBackend();
-  const [hoveredItem, setHoveredItem] = useLocalState('hoveredItem', {});
+  const { act } = useBackend(context);
+  const [hoveredItem, setHoveredItem] = useLocalState(context, 'hoveredItem', {});
   const hoveredCost = (hoveredItem && hoveredItem.cost) || 0;
   // Append extra hover data to items
   const items = props.items.map((item) => {
@@ -150,33 +211,6 @@ const ItemList = (props) => {
       disabled,
     };
   });
-  const GetTooltipMessage = (entry_name, is_illegal, are_contents_illegal) => {
-    if (is_illegal) {
-      return (
-        <Tooltip content="This product is powered by our latest technology. Please do not let Nanotrasen R&D steal our confidential designs.">
-          <Box inline position="relative" mr={1}>
-            {entry_name}
-          </Box>
-        </Tooltip>
-      );
-    } else if (are_contents_illegal) {
-      return (
-        <Tooltip content="The catalogue information is labeled as the product is implemented with our technology, but this may not be correct. If you're looking for a product with our technology, be careful of purchasing this.">
-          <Box inline position="relative" mr={1}>
-            {entry_name}
-          </Box>
-        </Tooltip>
-      );
-    } else {
-      return (
-        <Tooltip content="This product is not implemented with our technology.">
-          <Box inline position="relative" mr={1}>
-            {entry_name}
-          </Box>
-        </Tooltip>
-      );
-    }
-  };
   if (compactMode) {
     return (
       <Table>
@@ -207,11 +241,7 @@ const ItemList = (props) => {
   return items.map((item) => (
     <Section
       key={item.name}
-      title={GetTooltipMessage(
-        item.name,
-        item.is_illegal,
-        item.are_contents_illegal,
-      )}
+      title={item.name}
       level={2}
       buttons={
         <Button
@@ -225,8 +255,7 @@ const ItemList = (props) => {
             })
           }
         />
-      }
-    >
+      }>
       {decodeHtmlEntities(item.desc)}
     </Section>
   ));
